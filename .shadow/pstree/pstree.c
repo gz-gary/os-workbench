efc30aa3b7baf7f32680797bccd0ed9a9899bd84
tracer-nju <tracer@nju.edu.cn>
2024-03-02 20:23:54 +0800

    Linux XiaoXinAir 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:23:54 up 48 min,  1 user,  load average: 1.12, 0.92, 0.68

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,11 +1,48 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <assert.h>
+#include <dirent.h>
+#include <sys/procfs.h>
+
+int is_pure_digits(const char *str) {
+    int len = strlen(str);
+    for (int i = 0; i < len; ++i)
+        if (str[i] < '0' || str[i] > '9') return 0;
+    return 1;
+}
+
+typedef struct Process Process;
+
+int process_cnt;
+
+struct Process {
+    pid_t pid;
+    int id;
+    Process *parent;
+};
+
+void fetch_all_processes() {
+    DIR *dir = opendir("/proc");
+    if (dir == NULL) return;
+
+    FILE *fp = fopen("/proc/", "r");
+    struct dirent *entry;
+
+    while ((entry = readdir(dir)) != NULL) {
+        printf("%s\n", entry->d_name);
+    }
+}
 
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]);
-  return 0;
+    for (int i = 0; i < argc; i++) {
+        assert(argv[i]);
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    assert(!argv[argc]);
+
+    fetch_all_processes();
+  
+    return 0;
 }
