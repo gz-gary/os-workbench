afa320b41890bd123b6aaf865267ddc3b7485758
tracer-nju <tracer@nju.edu.cn>
2024-03-11 10:52:32 +0800

    Linux Zhongjinli2004 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:52:32 up  1:15,  0 users,  load average: 0.18, 0.34, 0.26

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -9,6 +9,11 @@
 #include "util_func.h"
 #include "parse_args.h"
 
+#define for_in_list(list_tail, node) for (ListNode *node = list_tail; node; node = node->prev)
+#define for_in_list_prev(list_tail, node, prev_node) for (ListNode *node = list_tail, *prev_node = node->prev; node; node = prev_node, prev_node = node->prev)
+#define insert_in_list(list_tail, node) list_tail = insert_item(list_tail, node)
+#define swap(type, a, b) type t = a; a = b; b = t;
+
 ListNode *processes_list_tail;
 Process *root_of_process_tree;
 
@@ -23,7 +28,7 @@ void fetch_one_process(const char *pid_str) {
     fscanf(fp, "%d %s %*c %d", &proc->pid, proc->name, &proc->ppid);
     proc->son_list_tail = NULL;
     remove_parentheses(proc->name);
-    processes_list_tail = insert_item(processes_list_tail, proc);
+    insert_in_list(processes_list_tail, proc);
 
 release_fp:
     if (fp) fclose(fp);
@@ -43,26 +48,24 @@ release_dir:
 }
 
 void buildup_process_tree() {
-    for (ListNode *now = processes_list_tail; now; now = now->prev) {
-        for (ListNode *parent = processes_list_tail; parent; parent = parent->prev)
-            if (parent->item->pid == now->item->ppid) {
-                parent->item->son_list_tail = insert_item(parent->item->son_list_tail, now->item);
+    for_in_list(processes_list_tail, proc) {
+        for_in_list(processes_list_tail, parent)
+            if (parent->item->pid == proc->item->ppid) {
+                insert_in_list(parent->item->son_list_tail, proc->item);
                 break;
             }
-        if (!now->item->ppid)
-            root_of_process_tree = now->item;
+        if (!proc->item->ppid)
+            root_of_process_tree = proc->item;
     }
 
     // using bubble sort for LinkList here
     if (flag_n) {
-        for (ListNode *now = processes_list_tail; now; now = now->prev) {
-            for (ListNode *p1 = now->item->son_list_tail; p1; p1 = p1->prev) {
-                ListNode *p2 = now->item->son_list_tail;
-                while (((p2->prev) != NULL) && ((p2->item)->pid) > (((p2->prev)->item)->pid)) {
-                    Process *t = p2->item;
-                    p2->item = (p2->prev)->item;
-                    (p2->prev)->item = t;
-                    p2 = p2->prev;
+        for_in_list(processes_list_tail, proc) {
+            for_in_list(proc->item->son_list_tail, _) {
+                for_in_list_prev(proc->item->son_list_tail, now, prev) {
+                    if (prev && now->item->pid > prev->item->pid) {
+                        swap(Process*, now->item, prev->item);
+                    }
                 }
             }
         }
@@ -76,14 +79,25 @@ void traverse_process_tree(Process *now, int depth) {
     if (flag_p) printf("(%d)", now->pid);
     printf("\n");
 
-    for (ListNode *son = now->son_list_tail; son; son = son->prev) {
+    for_in_list(now->son_list_tail, son)
         traverse_process_tree(son->item, depth + 1);
-    }
 }
 
 void show_all_processes() {
     for (ListNode *now = processes_list_tail; now; now = now->prev) {
         printf("%d %s\n", now->item->pid, now->item->name);
+    }
+}
+
+void cleanup() {
+    for_in_list_prev(processes_list_tail, proc, _) {
+
+        for_in_list_prev(proc->item->son_list_tail, son, _) {
+            free(son);
+        }
+
+        free(proc->item);
+        free(proc);
     }
 }
 
@@ -103,7 +117,7 @@ int main(int argc, char *argv[]) {
     fetch_all_processes();
     buildup_process_tree();
     traverse_process_tree(root_of_process_tree, 0);
-    // show_all_processes();
+    //cleanup();
   
     return 0;
 }
