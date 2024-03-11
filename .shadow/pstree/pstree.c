2e96adc1e95d13bcb7c70635f2a9fdc1fdf0a8ce
tracer-nju <tracer@nju.edu.cn>
2024-03-11 11:00:32 +0800

    Linux Zhongjinli2004 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:00:32 up  1:23,  0 users,  load average: 0.11, 0.18, 0.21

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -12,7 +12,7 @@
 #define for_in_list(list_tail, node) for (ListNode *node = list_tail; node; node = node->prev)
 #define for_in_list_prev(list_tail, node, prev_node) for (ListNode *node = list_tail, *prev_node = node->prev; node; node = prev_node, prev_node = (node ? node->prev : NULL))
 #define insert_in_list(list_tail, node) list_tail = insert_item(list_tail, node)
-#define swap(type, a, b) type t = a; a = b; b = t;
+#define swap(type, a, b) { type t = a; a = b; b = t; }
 
 ListNode *processes_list_tail;
 Process *root_of_process_tree;
@@ -59,17 +59,12 @@ void buildup_process_tree() {
     }
 
     // using bubble sort for LinkList here
-    if (flag_n) {
-        for_in_list(processes_list_tail, proc) {
-            for_in_list(proc->item->son_list_tail, _) {
-                for_in_list_prev(proc->item->son_list_tail, now, prev) {
-                    if (prev && now->item->pid < prev->item->pid) {
+    if (flag_n)
+        for_in_list(processes_list_tail, proc)
+            for_in_list(proc->item->son_list_tail, _)
+                for_in_list_prev(proc->item->son_list_tail, now, prev)
+                    if (prev && now->item->pid > prev->item->pid)
                         swap(Process*, now->item, prev->item);
-                    }
-                }
-            }
-        }
-    }
 
 }
 
@@ -117,7 +112,7 @@ int main(int argc, char *argv[]) {
     fetch_all_processes();
     buildup_process_tree();
     traverse_process_tree(root_of_process_tree, 0);
-    //cleanup();
+    cleanup();
   
     return 0;
 }
