e6956e3040dabe9ee2cc91918f97127d792c79e1
tracer-nju <tracer@nju.edu.cn>
2024-03-11 11:29:24 +0800

    Linux Zhongjinli2004 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:29:24 up  1:52,  0 users,  load average: 0.06, 0.12, 0.14

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -138,7 +138,7 @@ void buildup_process_tree() {
         for_in_list(processes_list_tail, proc)
             for_in_list(proc->item->son_list_tail, _)
                 for_in_list_prev(proc->item->son_list_tail, now, prev)
-                    if (prev && now->item->pid < prev->item->pid)
+                    if (prev && now->item->pid > prev->item->pid)
                         swap(Process*, now->item, prev->item);
 
 }
