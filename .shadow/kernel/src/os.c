5894f65d774633351b03a12be49ab5e34c91bc86
tracer-nju <tracer@nju.edu.cn>
2024-04-09 23:32:14 +0800

    Linux ubuntu 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:32:14 up  3:19,  1 user,  load average: 0.26, 0.32, 0.35

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -1,7 +1,5 @@
-//#include <assert.h>
-#include "am.h"
-#include <spinlock.h>
 #include <common.h>
+#include <spinlock.h>
 
 int shared_counter;
 spinlock_t big_kernel_lock;
@@ -18,12 +16,12 @@ static void os_run() {
     spinlock_init(&big_kernel_lock);
     while (shared_counter <= 300) {
         spinlock_lock(&big_kernel_lock);
-        if (big_kernel_lock.owner != cpu_current()) {
-            printf("no\n");
-        }
-        assert(big_kernel_lock.owner == cpu_current());
-        //++shared_counter;
-        //printf("cpu%d add counter to -> %d\n", cpu_current(), shared_counter);
+        //if (big_kernel_lock.owner != cpu_current()) {
+            //printf("no\n");
+        //}
+        //assert(big_kernel_lock.owner == cpu_current());
+        ++shared_counter;
+        printf("cpu%d add counter to -> %d\n", cpu_current(), shared_counter);
         spinlock_unlock(&big_kernel_lock);
     }
     while (1) ;
