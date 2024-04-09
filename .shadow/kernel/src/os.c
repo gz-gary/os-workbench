7019f795dedd4faff67caada70485f39cfda1c21
tracer-nju <tracer@nju.edu.cn>
2024-04-09 23:38:02 +0800

    Linux ubuntu 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:38:02 up  3:25,  1 user,  load average: 1.47, 1.02, 0.67

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -7,13 +7,13 @@ spinlock_t big_kernel_lock;
 static void os_init() {
     pmm->init();
     spinlock_init(&big_kernel_lock);
+    shared_counter = 0;
 }
 
 static void os_run() {
     //for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
         //putch(*s == '*' ? '0' + cpu_current() : *s);
     //}
-    shared_counter = 0;
     while (shared_counter <= 300) {
         spinlock_lock(&big_kernel_lock);
         assert(big_kernel_lock.owner == cpu_current());
