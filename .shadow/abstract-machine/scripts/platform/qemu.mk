0c9ecbf86bde9e47ef827067a856146a6df1977b
tracer-nju <tracer@nju.edu.cn>
2024-04-09 22:56:08 +0800

    Linux ubuntu 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:56:08 up  2:43,  1 user,  load average: 0.39, 0.38, 0.30

--- .shadow/abstract-machine/scripts/platform/qemu.mk
+++ .shadow/abstract-machine/scripts/platform/qemu.mk
@@ -1,12 +1,13 @@
 .PHONY: build-arg
 
 smp        ?= 1
-smp        := 1
 LDFLAGS    += -N -Ttext-segment=0x00100000
 QEMU_FLAGS += -serial mon:stdio \
               -machine accel=tcg \
               -smp "$(smp),cores=$(smp),sockets=1" \
               -drive format=raw,file=$(IMAGE)
+noname:
+	echo $(smp)
 
 build-arg: image
 	@( echo -n $(mainargs); ) | dd if=/dev/stdin of=$(IMAGE) bs=512 count=2 seek=1 conv=notrunc status=none
