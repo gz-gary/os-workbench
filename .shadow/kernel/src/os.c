6e648affc9ceb44ef288c67ec334f57a02c150d7
tracer-nju <tracer@nju.edu.cn>
2024-04-09 23:23:13 +0800

    Linux ubuntu 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:23:13 up  3:10,  1 user,  load average: 0.33, 0.35, 0.34

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -1,14 +1,26 @@
+#include "am.h"
+#include <spinlock.h>
 #include <common.h>
+
+int shared_counter;
+spinlock_t big_kernel_lock;
 
 static void os_init() {
     pmm->init();
 }
 
 static void os_run() {
-    for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
-        putch(*s == '*' ? '0' + cpu_current() : *s);
+    //for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+        //putch(*s == '*' ? '0' + cpu_current() : *s);
+    //}
+    shared_counter = 0;
+    spinlock_init(&big_kernel_lock);
+    while (shared_counter <= 300) {
+        spinlock_lock(&big_kernel_lock);
+        ++shared_counter;
+        printf("cpu%d add counter to -> %d\n", cpu_current(), shared_counter);
+        spinlock_unlock(&big_kernel_lock);
     }
-    printf("%d\n", cpu_count());
     while (1) ;
 }
 
