db5483dedea06f08a5566e40222bdf1c763314eb
tracer-nju <tracer@nju.edu.cn>
2024-04-09 21:54:58 +0800

    Linux ubuntu 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:54:58 up  1:41,  1 user,  load average: 0.24, 0.40, 0.33

--- .shadow/kernel/framework/main.c
+++ .shadow/kernel/framework/main.c
@@ -3,13 +3,8 @@
 #include <kernel.h>
 #include <klib.h>
 
-static void klib_printf_test() {
-    printf("%d ha%dha %d %d %d %s\n", 6657, -2147483648, 2147483647, -553, 0, "wocenidemaasd");
-}
-
 int main() {
-    klib_printf_test();
-    //os->init();
+    os->init();
     //mpe_init(os->run);
     return 1;
 }
