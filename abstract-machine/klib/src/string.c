5ff3711edc983d3180b1d998a72e5dbab907cbf2
gz <jinlizhong@smail.nju.edu.cn>
2024-04-09 22:07:43 +0800

    finish memset

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -40,7 +40,8 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  for (; n > 0; --n, ++s) *(unsigned char *)s = (unsigned char)c;
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
