a416f8b61d329ba3dd2c2f5e6dd857b2a2009b0a
tracer-nju <tracer@nju.edu.cn>
2024-04-10 09:25:20 +0800

    Linux ubuntu 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:25:20 up 17 min,  1 user,  load average: 0.17, 0.22, 0.19

--- .shadow/kernel/include/fslist.h
+++ .shadow/kernel/include/fslist.h
@@ -1,6 +1,8 @@
 
+#include <stddef.h>
 typedef struct ls_node ls_node;
 
 struct ls_node {
+    size_t len;
     ls_node *next;
 };
--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -11,24 +11,8 @@ static void os_init() {
 }
 
 static void os_run() {
-    //for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
-        //putch(*s == '*' ? '0' + cpu_current() : *s);
-    //}
-    while (1) {
-        spinlock_lock(&big_kernel_lock);
-
-        assert(big_kernel_lock.owner == cpu_current());
-
-        if (shared_counter < 300) {
-            ++shared_counter;
-            printf("cpu%d add counter to -> %d\n", cpu_current(), shared_counter);
-            spinlock_unlock(&big_kernel_lock);
-            continue;
-        } else {
-            spinlock_unlock(&big_kernel_lock);
-            break;
-        }
-
+    for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+        putch(*s == '*' ? '0' + cpu_current() : *s);
     }
     while (1) ;
 }
