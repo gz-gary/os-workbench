e90a179a664877e3fbe107546d53dd1c72388a0f
tracer-nju <tracer@nju.edu.cn>
2024-04-12 15:28:35 +0800

    Linux ubuntu 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:28:35 up  1:19,  0 users,  load average: 0.17, 0.26, 0.22

--- .shadow/kernel/test/am.h
+++ .shadow/kernel/test/am.h
@@ -1,4 +1,3 @@
-#pragma once
 #include <stdio.h>
 #include <stdint.h>
 
@@ -6,16 +5,9 @@ struct heap_t {
     uintptr_t start, end;
 };
 
-inline void putch(char ch) {
-    putchar(ch);
-}
+extern inline void putch(char ch);
 
 extern int cpu_current();
 extern int cpu_count();
 
-inline int atomic_xchg(volatile int *addr, int newval) {
-    int result;
-    asm volatile ("lock xchg %0, %1":
-        "+m"(*addr), "=a"(result) : "1"(newval) : "memory");
-    return result;
-}
+extern inline int atomic_xchg(volatile int *addr, int newval);
