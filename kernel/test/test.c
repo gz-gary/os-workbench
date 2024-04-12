fb01673cc5c46ed6b2b4bf342a27a5e86c6c2ec3
gz <jinlizhong@smail.nju.edu.cn>
2024-04-12 15:45:17 +0800

    build testing framework

---
+++ kernel/test/test.c
@@ -0,0 +1,40 @@
+#include <thread.h>
+#include <kernel.h>
+
+#define NR_CPUS 4
+
+int cpu_current() {
+    pthread_t pid = pthread_self();
+    for (int i = 0; i < n_; ++i) {
+        if (threads_[i].thread == pid) {
+            return threads_[i].id;
+        }
+    }
+}
+
+int cpu_count() {
+    return n_;
+}
+
+inline void putch(char ch) {
+    putchar(ch);
+}
+
+inline int atomic_xchg(volatile int *addr, int newval) {
+    int result;
+    asm volatile ("lock xchg %0, %1":
+        "+m"(*addr), "=a"(result) : "1"(newval) : "memory");
+    return result;
+}
+
+static void entry(int id) {
+    while (n_ < NR_CPUS); //wait until all threads were created
+    pmm->alloc(128);
+}
+
+int main() {
+    pmm->init();
+    for (int i = 0; i < 4; ++i) {
+        create(entry);
+    }
+}
