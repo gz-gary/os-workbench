5203fa511d4929de32bdc6f43fd3b7f36b6d9635
tracer-nju <tracer@nju.edu.cn>
2024-04-12 17:23:40 +0800

    Linux ubuntu 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:23:40 up  3:14,  0 users,  load average: 0.23, 0.24, 0.24

--- .shadow/kernel/test/test.c
+++ .shadow/kernel/test/test.c
@@ -47,33 +47,33 @@ static void entry(int id) {
     size_t block_size[1];
     void *ptr[1];
     size_t max_size = 8; //1 B to 64 KiB per request
+
     for (int i = 0; i < LENGTH(block_size); ++i) {
         block_size[i] = rand() % max_size + 1;
         ptr[i] = pmm->alloc(block_size[i]);
+
         // check if we get an available addr
         assert(ptr[i]);
         // check if we get a legal addr
         assert(((uintptr_t)ptr[i] & (power_bound(block_size[i]) - 1)) == 0);
     }
+
     unsigned char my_identifier = id;
     for (int i = 0; i < LENGTH(block_size); ++i) {
         // brush my range with my id
         memset(ptr[i], my_identifier, block_size[i]);
     }
-    // check if anyone invades my range for k times
-    for (int k = 0; k < 10; ++k) {
+
+    // check if anyone invades my range for 10 times
+    for (int k = 0; k < 10; ++k)
         for (int i = 0; i < LENGTH(block_size); ++i) {
             for (size_t offset = 0; offset < block_size[i]; ++offset) {
                 unsigned char byte_here = *(unsigned char *)(ptr[i] + offset);
-                ECHO_VAR(byte_here, %d);
-                ECHO_VAR(id, %d);
                 assert(byte_here == id);
             }
         }
-    }
-    for (int i = 0; i < LENGTH(block_size); ++i) {
-        pmm->free(ptr[i]);
-    }
+
+    for (int i = 0; i < LENGTH(block_size); ++i) pmm->free(ptr[i]);
 }
 
 static void alloc_test() {
