487eb9617069002950563c4a5455a95d46394fcc
tracer-nju <tracer@nju.edu.cn>
2024-04-13 08:48:10 +0800

    Linux Zhongjinli2004 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  08:48:10 up 56 min,  0 users,  load average: 0.17, 0.16, 0.07

--- .shadow/kernel/include/util-funcs.h
+++ .shadow/kernel/include/util-funcs.h
@@ -1,13 +1,13 @@
 #include <stdint.h>
 #include <sys/types.h>
 
-inline size_t power_bound(size_t x) {
+static inline size_t power_bound(size_t x) {
     size_t ret = 1;
     while (ret < x) ret <<= 1;
     return ret;
 }
 
-inline void *align_to_bound(void *ptr, size_t bound) {
+static inline void *align_to_bound(void *ptr, size_t bound) {
     void *next_available = (void *)(
         (((uintptr_t)ptr - 1) & (~(bound - 1)))
         + bound);
