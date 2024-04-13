a7e334a30f4fbd2ec55da2aac9a39b02b7e22507
tracer-nju <tracer@nju.edu.cn>
2024-04-13 09:51:46 +0800

    Linux Zhongjinli2004 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:51:46 up  2:00,  0 users,  load average: 0.34, 0.30, 0.24

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -71,7 +71,6 @@ static void setup_heap_structure() {
     nr_page >>= 1;
 
     chunks = heap.start;
-    assert(chunks == heap.start);
     chunklist = heap.start + nr_page * sizeof(chunk_t);
     mem = align_to_bound(chunklist + (log_nr_page + 1) * sizeof(chunklist_t),
                          nr_page << LOG_PAGE_SIZE);
@@ -79,8 +78,8 @@ static void setup_heap_structure() {
 
     printf("\nwe make heap to this structure:\n\n");
     printf("Manage %ld pages\n", nr_page);
-    printf("[%p, %p) to store chunks\n", chunks, chunks + nr_page * sizeof(chunk_t));
-    printf("[%p, %p) to store chunklist\n", chunklist, chunklist + (log_nr_page + 1) * sizeof(chunklist_t));
+    printf("[%p, %p) to store chunks\n", chunks, chunks + nr_page);
+    printf("[%p, %p) to store chunklist\n", chunklist, chunklist + (log_nr_page + 1));
     printf("[%p, %p) to allocate\n", mem, mem + nr_page * PAGE_SIZE);
 }
 
