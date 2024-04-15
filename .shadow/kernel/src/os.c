9ca47792106cd2560f7c342c327f562a7d52cf5d
tracer-nju <tracer@nju.edu.cn>
2024-04-15 21:09:40 +0800

    Linux ubuntu 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:40 up 22 min,  1 user,  load average: 0.39, 0.15, 0.12

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -1,13 +1,63 @@
 #include <common.h>
+#include <spinlock.h>
+
+#define NR_CPUS 4
+#define BUF 512
+#define TOTAL_ALLOC 120
+
+typedef struct workload_t workload_t;
+typedef struct workload_queue_t workload_queue_t;
+
+struct workload_t {
+    enum {
+        WORK_ALLOC,
+        WORK_FREE
+    } type;
+    union {
+        void *ptr;
+        size_t size;
+    };
+};
+
+struct workload_queue_t {
+    spinlock_t lock;
+    int head, tail, size;
+    workload_t queue[BUF];
+};
+
+workload_queue_t consumer_queue[NR_CPUS];
+
+void queue_init(workload_queue_t *q) {
+    spinlock_init(&q->lock);
+    q->head = 0;
+    q->tail = 0;
+    q->size = 0;
+}
+
+void queue_push(workload_queue_t *q, workload_t workload) {
+    q->queue[q->tail] = workload;
+    q->tail = (q->tail + 1) % BUF;
+    ++q->size;
+}
+
+workload_t queue_pop(workload_queue_t *q) {
+    workload_t ret = q->queue[q->head];
+    q->head = (q->head + 1) % BUF;
+    --q->size;
+    return ret;
+}
 
 static void os_init() {
     pmm->init();
+    for (int i = 0; i < NR_CPUS; ++i) {
+        queue_init(&consumer_queue[i]);
+    }
 }
 
 static void os_run() {
-    for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+    /*for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
         putch(*s == '*' ? '0' + cpu_current() : *s);
-    }
+    }*/
     while (1) ;
 }
 
