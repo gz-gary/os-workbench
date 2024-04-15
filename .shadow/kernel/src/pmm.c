ceea598035b324c0da2c5d3331d0e16e0f5ecb9a
tracer-nju <tracer@nju.edu.cn>
2024-04-15 09:34:01 +0800

    Linux Zhongjinli2004 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:34:01 up  1:05,  0 users,  load average: 0.09, 0.18, 0.14

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -75,7 +75,7 @@ static void setup_heap_layout() {
 
     chunks = heap.start;
     chunklist = (void *)chunks + nr_page * sizeof(chunk_t);
-    slabs = (void *)chunklist + (cpu_count() * (SLAB_LEVEL_MAXIMAL - SLAB_LEVEL_MINIMAL + 1)) * sizeof(slab_t);
+    slabs = (void *)chunklist + (log_nr_page + 1) * sizeof(chunklist_t);
     mem = align_to_bound(chunklist + (log_nr_page + 1) * sizeof(chunklist_t),
                          nr_page << LOG_PAGE_SIZE);
 
