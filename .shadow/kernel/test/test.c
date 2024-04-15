226b1d4231541f8e55c97abe5e5d774eba01402f
tracer-nju <tracer@nju.edu.cn>
2024-04-15 11:01:03 +0800

    Linux Zhongjinli2004 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:01:03 up  2:32,  0 users,  load average: 0.17, 0.11, 0.14

--- .shadow/kernel/test/test.c
+++ .shadow/kernel/test/test.c
@@ -44,8 +44,8 @@ static inline size_t power_bound(size_t x) {
 static void entry(int id) {
     while (n_ < NR_CPUS); //wait until all threads were created
 
-    size_t block_size[8];
-    void *ptr[8];
+    size_t block_size[1];
+    void *ptr[1];
     //size_t max_size = 64 * 1024; //1 B to 64 KiB per request
     size_t min_size = 128;
 
@@ -95,6 +95,6 @@ static void simple_test() {
 int main() {
     srand(time(0));
     pmm->init();
-    alloc_test();
-    //simple_test();
+    // alloc_test();
+    simple_test();
 }
