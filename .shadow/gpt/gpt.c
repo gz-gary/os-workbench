ce25dd23f15981899a5c9c8d54a7d5bcd714f225
tracer-nju <tracer@nju.edu.cn>
2024-05-04 15:44:05 +0800

    Linux gz-ubuntu 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:44:05 up  5:46,  1 user,  load average: 0.75, 0.73, 0.57

--- .shadow/gpt/gpt.c
+++ .shadow/gpt/gpt.c
@@ -186,6 +186,7 @@ void matmul_forward(float* out,
             pthread_join(worker[i], NULL);
         }
     } else {
+        if (OC <= 4) assert(0);
         for (int i = 0; i < 4; ++i) {
             workload[i].OC_l = i * (OC / 4);
             workload[i].OC_r = (i + 1) * (OC / 4);
