d45edb12968c25878d5f03ee76b1548eab42cffc
tracer-nju <tracer@nju.edu.cn>
2024-05-04 15:05:30 +0800

    Linux gz-ubuntu 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:05:30 up  5:07,  1 user,  load average: 0.91, 0.54, 0.21

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -70,12 +70,6 @@ static void setup_heap_layout() {
 
         if (temp_log < 0) break;
         else {
-            
-            printf("ahead %d %d\n", temp_log, 1 << temp_log);
-            printf("chunks extend [%p, %p)\n", ((void*)chunks + nr_page * sizeof(chunk_t)),
-                                               ((void*)chunks + (nr_page + (1 << temp_log)) * sizeof(chunk_t)));
-            printf("mem extend [%p, %p)\n", (mem + nr_page * PAGE_SIZE),
-                                            (mem + (nr_page + (1 << temp_log)) * PAGE_SIZE));
             for (size_t i = nr_page; i < nr_page + (1 << temp_log); ++i)
                 chunks[i] = (chunk_t){
                     .status = CHUNK_FREE,
