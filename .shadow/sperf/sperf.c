743be3d63fe61ce34f706ee0f02ba1198fd306d6
tracer-nju <tracer@nju.edu.cn>
2024-05-07 20:55:58 +0800

    Linux gz-ubuntu 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:55:58 up  1:42,  1 user,  load average: 0.78, 0.59, 0.50

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/syscall.h>
@@ -14,11 +15,15 @@ int main(int argc, char *argv[]) {
     }
     assert(!argv[argc]);*/
     int fid = syscall(SYS_open, "log.txt", O_CREAT | O_WRONLY | O_TRUNC);
-    dprintf(fid, "some content\n");
-    close(fid);
-    /*int pipeid[2];
-    assert(!pipe(pipeid));
-    if (fork() == 0) {
+    assert(fid);
+    dprintf(fid, "--- strace log below ---\n\n");
+
+    int pid = fork();
+    if (pid == 0) {
+        syscall(SYS_close, 1);
+        syscall(SYS_dup, fid);
+        syscall(SYS_close, fid);
+
         char *exec_argv[] = { "strace", "ls", NULL, };
         char *exec_envp[] = { "PATH=/bin", NULL, };
         execve("strace",          exec_argv, exec_envp);
@@ -26,6 +31,11 @@ int main(int argc, char *argv[]) {
         execve("/usr/bin/strace", exec_argv, exec_envp);
         perror(argv[0]);
         exit(EXIT_FAILURE);
-    }*/
+    }
+    int status;
+    pid = wait(&status);
+    assert(!WEXITSTATUS(status)); // strace exits normally
+
+    syscall(SYS_close, fid);
     return 0;
 }
