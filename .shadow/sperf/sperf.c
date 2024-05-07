58897c694e9fb524e83cee005b1edf7d0a142160
tracer-nju <tracer@nju.edu.cn>
2024-05-07 21:15:09 +0800

    Linux gz-ubuntu 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:15:09 up  2:01,  1 user,  load average: 0.20, 0.62, 0.59

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -19,12 +19,10 @@ int main(int argc, char *argv[]) {
     memcpy(exec_argv, argv, argc * sizeof(char *));
     exec_argv[0] = "strace";
     exec_argv[argc] = NULL;
-    for (int i = 0; i < argc; ++i) {
-        assert(exec_argv[i]);
-        printf("exec_argv[%d] = %s\n", i, exec_argv[i]);
-    }
+
+    char *exec_envp[] = { "PATH=/bin", NULL, };
     
-    /*int fid = open("log.txt", O_CREAT | O_WRONLY | O_TRUNC);
+    int fid = open("log.txt", O_CREAT | O_WRONLY | O_TRUNC);
     assert(fid);
     dprintf(fid, "--- strace log below ---\n\n");
 
@@ -34,19 +32,17 @@ int main(int argc, char *argv[]) {
         syscall(SYS_dup, fid);
         close(fid);
 
-        char *exec_argv[] = { "strace", argv[1], NULL, };
-        char *exec_envp[] = { "PATH=/bin", NULL, };
         execve("strace",          exec_argv, exec_envp);
         execve("/bin/strace",     exec_argv, exec_envp);
         execve("/usr/bin/strace", exec_argv, exec_envp);
-        perror(argv[0]);
+        perror(exec_argv[0]);
         exit(EXIT_FAILURE);
     }
     int status;
     pid = wait(&status);
     assert(!WEXITSTATUS(status)); // strace exits normally
 
+    close(fid);
     free(exec_argv);
-    close(fid);*/
     return 0;
 }
