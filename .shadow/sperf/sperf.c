6d0d62d213845a4dc261edfde75dc91e937f7a43
tracer-nju <tracer@nju.edu.cn>
2024-05-07 21:12:11 +0800

    Linux gz-ubuntu 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:12:11 up  1:58,  1 user,  load average: 1.07, 0.87, 0.65

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -14,7 +15,16 @@ int main(int argc, char *argv[]) {
         printf("argv[%d] = %s\n", i, argv[i]);
     }
     assert(!argv[argc]);*/
-    int fid = syscall(SYS_open, "log.txt", O_CREAT | O_WRONLY | O_TRUNC);
+    char **exec_argv = malloc((argc + 1) * sizeof(char *));
+    memcpy(exec_argv, argv, argc);
+    exec_argv[0] = "strace";
+    exec_argv[argc] = NULL;
+    for (int i = 0; i < argc; ++i) {
+        assert(exec_argv[i]);
+        printf("exec_argv[%d] = %s\n", i, argv[i]);
+    }
+    
+    /*int fid = open("log.txt", O_CREAT | O_WRONLY | O_TRUNC);
     assert(fid);
     dprintf(fid, "--- strace log below ---\n\n");
 
@@ -22,9 +32,9 @@ int main(int argc, char *argv[]) {
     if (pid == 0) {
         syscall(SYS_close, 2);
         syscall(SYS_dup, fid);
-        syscall(SYS_close, fid);
+        close(fid);
 
-        char *exec_argv[] = { "strace", "ls", NULL, };
+        char *exec_argv[] = { "strace", argv[1], NULL, };
         char *exec_envp[] = { "PATH=/bin", NULL, };
         execve("strace",          exec_argv, exec_envp);
         execve("/bin/strace",     exec_argv, exec_envp);
@@ -36,6 +46,7 @@ int main(int argc, char *argv[]) {
     pid = wait(&status);
     assert(!WEXITSTATUS(status)); // strace exits normally
 
-    syscall(SYS_close, fid);
+    free(exec_argv);
+    close(fid);*/
     return 0;
 }
