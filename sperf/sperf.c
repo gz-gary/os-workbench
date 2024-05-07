97daa264814504d2691625291ca4fc9d4afad112
gz <jinlizhong@smail.nju.edu.cn>
2024-05-07 20:59:01 +0800

    log to file

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/syscall.h>
@@ -15,10 +16,14 @@ int main(int argc, char *argv[]) {
     assert(!argv[argc]);*/
     int fid = syscall(SYS_open, "log.txt", O_CREAT | O_WRONLY | O_TRUNC);
     assert(fid);
-    dprintf(fid, "--- strace log below ---\n");
+    dprintf(fid, "--- strace log below ---\n\n");
 
     int pid = fork();
     if (pid == 0) {
+        syscall(SYS_close, 2);
+        syscall(SYS_dup, fid);
+        syscall(SYS_close, fid);
+
         char *exec_argv[] = { "strace", "ls", NULL, };
         char *exec_envp[] = { "PATH=/bin", NULL, };
         execve("strace",          exec_argv, exec_envp);
@@ -27,7 +32,10 @@ int main(int argc, char *argv[]) {
         perror(argv[0]);
         exit(EXIT_FAILURE);
     }
+    int status;
+    pid = wait(&status);
+    assert(!WEXITSTATUS(status)); // strace exits normally
 
-    close(fid);
+    syscall(SYS_close, fid);
     return 0;
 }
