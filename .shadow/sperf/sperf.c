d09f774c2ca5b2ca673315b81b97e93d23be54f0
tracer-nju <tracer@nju.edu.cn>
2024-05-07 20:41:37 +0800

    Linux gz-ubuntu 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:41:37 up  1:28,  1 user,  load average: 0.41, 0.38, 0.41

---
+++ .shadow/sperf/sperf.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+
+int main(int argc, char *argv[]) {
+    /*for (int i = 0; i < argc; i++) {
+        assert(argv[i]);
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    assert(!argv[argc]);*/
+    int fid = syscall(SYS_open, "log.txt", O_CREAT | O_WRONLY | O_TRUNC);
+    dprintf(fid, "some content\n");
+    close(fid);
+    /*int pipeid[2];
+    assert(!pipe(pipeid));
+    if (fork() == 0) {
+        char *exec_argv[] = { "strace", "ls", NULL, };
+        char *exec_envp[] = { "PATH=/bin", NULL, };
+        execve("strace",          exec_argv, exec_envp);
+        execve("/bin/strace",     exec_argv, exec_envp);
+        execve("/usr/bin/strace", exec_argv, exec_envp);
+        perror(argv[0]);
+        exit(EXIT_FAILURE);
+    }*/
+    return 0;
+}
