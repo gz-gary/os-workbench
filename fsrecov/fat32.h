1a745b2283e688c7800e63009aa5370b2a4a683f
gz <jinlizhong@smail.nju.edu.cn>
2024-07-03 22:19:53 +0800

    finish baseline

--- fsrecov/fat32.h
+++ fsrecov/fat32.h
@@ -51,6 +51,30 @@ struct fat32dent {
     u32 DIR_FileSize;
 } __attribute__((packed));
 
+struct fat32ldent {
+    u8  LDIR_Ord;
+    u16 LDIR_Name1[5];
+    u8  LDIR_Attr;
+    u8  LDIR_Type;
+    u8  LDIR_Chksum;
+    u16 LDIR_Name2[6];
+    u16 LDIR_FstClusLO;
+    u16 LDIR_Name3[2];
+} __attribute__((packed));
+
+typedef enum {
+    CLUS_DENT = 0,
+    CLUS_BMPHDR,
+    CLUS_BMPDATA,
+    CLUS_OTHERS
+} clus_type_t;
+/*char idstr[4][20] = {
+    "CLUS_DENT",
+    "CLUS_BMPHDR",
+    "CLUS_BMPDATA",
+    "CLUS_OTHERS"
+};*/
+
 #define CLUS_INVALID   0xffffff7
 
 #define ATTR_READ_ONLY 0x01
@@ -59,3 +83,33 @@ struct fat32dent {
 #define ATTR_VOLUME_ID 0x08
 #define ATTR_DIRECTORY 0x10
 #define ATTR_ARCHIVE   0x20
+
+#define ATTR_LONG_NAME (ATTR_READ_ONLY | ATTR_HIDDEN | ATTR_SYSTEM | ATTR_VOLUME_ID)
+#define ATTR_LONG_NAME_MASK (ATTR_READ_ONLY | ATTR_HIDDEN | ATTR_SYSTEM | ATTR_VOLUME_ID | ATTR_DIRECTORY | ATTR_ARCHIVE)
+
+#define MAX_CLUS 20000
+#define LAST_LONG_ENTRY 0x40
+
+struct bmp_pixel {
+    u8 b, g, r;
+} __attribute__((packed));
+
+struct bmp_hdr_t {
+    u16   BMP_Magic;
+    u32   BMP_FileSz;
+    u16   BMP_Rsvd1;
+    u16   BMP_Rsvd2;
+    u32   BMP_DataOff;
+    u32   BMP_DIBHdrSz;
+    u32   BMP_Width;
+    u32   BMP_Height;
+    u16   BMP_Planes;
+    u16   BMP_BitsPerPixel;
+    u32   BMP_Compress;
+    u32   BMP_ImgSz;
+    u32   BMP_XResol;
+    u32   BMP_YResol;
+    u32   BMP_Cols;
+    u32   BMP_ImportantCols;
+    struct bmp_pixel pixels[0];
+} __attribute__((packed));
