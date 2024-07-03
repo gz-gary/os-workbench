fbec26b888840e4128a1dc6f9c446b5466583b5d
tracer-nju <tracer@nju.edu.cn>
2024-07-03 19:44:36 +0800

    Linux pc 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:44:36 up 10:50,  7 users,  load average: 0.10, 0.15, 0.22

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -137,14 +137,13 @@ void dump_bmp() {
 				int len = 0;
 				char buf[64];
 				for (int j = idx - 1; j >= 0; --j) {
-					len = 0;
 					for (int k = 1; k <= 5; ++k) buf[len++] = ldent[j].LDIR_Name1[k - 1];
 					for (int k = 6; k <= 11; ++k) buf[len++] = ldent[j].LDIR_Name2[k - 6];
 					for (int k = 12; k <= 13; ++k) buf[len++] = ldent[j].LDIR_Name3[k - 12];
-					buf[len++] = '\0';
-					if (ascii_printable(buf, len - 1)) {
-						printf("[Long file name]: %s\n", buf);
-					}
+				}
+				buf[len++] = '\0';
+				if (ascii_printable(buf, len - 1)) {
+					printf("[Long file name]: %s\n", buf);
 				}
 				i += idx;
 			} else { // this entry is a 'short name directory entry'
