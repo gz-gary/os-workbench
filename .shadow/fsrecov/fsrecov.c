51990b47a72334db3eb3991dd0ccdf1a0415164a
tracer-nju <tracer@nju.edu.cn>
2024-07-03 21:36:45 +0800

    Linux pc 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:36:45 up 12:42,  7 users,  load average: 0.50, 0.36, 0.26

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -201,6 +201,7 @@ void dump_bmp() {
 			printf("W=%u H=%u", bmp_hdr->BMP_Width, bmp_hdr->BMP_Height);
 
 			char tmp_file_name[256];
+			bzero(tmp_file_name, 256);
 			sprintf(tmp_file_name, "/tmp/fsrecov/%s", buf);
 			int bmp_fd = open(tmp_file_name, O_RDWR | O_CREAT, 0666);
 			for (int j = 0; j < bmp_cnt_clus; ++j) {
