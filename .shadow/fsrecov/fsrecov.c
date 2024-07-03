0455b39fcad2ed6701ca9ff4df40e10aefdd1a98
tracer-nju <tracer@nju.edu.cn>
2024-07-03 11:30:21 +0800

    Linux pc 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:30:21 up  2:36,  6 users,  load average: 0.25, 0.30, 0.27

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include "fat32.h"
+#define MAX_CLUS 20000
 
 #define ATTR_LONG_NAME (ATTR_READ_ONLY | ATTR_HIDDEN | ATTR_SYSTEM | ATTR_VOLUME_ID)
 
@@ -22,6 +23,10 @@ struct fat32dent_long {
 } __attribute__((packed));
 
 struct fat32hdr *hdr;
+u32 bytes_per_clus;
+u8 before_data_sec;
+u8 *clus_begin;
+u8 *clus_end;
 
 void* mmap_disk(const char *filename);
 void dump_bmp();
@@ -75,10 +80,49 @@ release:
     exit(1);
 }
 
+typedef enum {
+	CLUS_DENT = 0,
+	CLUS_BMPHDR,
+	CLUS_BMPDATA,
+	CLUS_OTHERS
+} clus_type_t;
+char idstr[4][20] = {
+	"CLUS_DENT",
+	"CLUS_BMPHDR",
+	"CLUS_BMPDATA",
+	"CLUS_OTHERS"
+};
+
+clus_type_t clus_type[MAX_CLUS];
+
+clus_type_t probe_clus_type(u8 *clus) {
+	if (clus[0] == 'B' && clus[1] == 'M') return CLUS_BMPHDR;
+	int cnt_bmp = 0, cnt_zero = 0;
+
+	for (int i = 0; i < bytes_per_clus - 2; ++i) {
+		if ((clus[i] == 'B' && clus[i + 1] == 'M' && clus[i + 2] == 'P') ||
+			(clus[i] == 'b' && clus[i + 1] == 'm' && clus[i + 2] == 'p')) {
+			++cnt_bmp;
+		}
+	}
+
+	for (int i = 0; i < bytes_per_clus; ++i) if (clus[i] == 0) ++cnt_zero;
+
+	if (cnt_bmp >= 3) return CLUS_DENT;
+	if (cnt_zero == bytes_per_clus) return CLUS_OTHERS;
+	return CLUS_BMPDATA;
+}
+
 void dump_bmp() {
-	u32 bytes_per_clus   = hdr->BPB_SecPerClus * hdr->BPB_BytsPerSec;
-	u8 before_data_sec   = hdr->BPB_RsvdSecCnt + (hdr->BPB_NumFATs * hdr->BPB_FATSz32);
-	u8 *clus_begin       = (u8 *)hdr + before_data_sec * hdr->BPB_BytsPerSec;
-	u8 *clus_end         = (u8 *)hdr + hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec;
-	printf("%u\n", hdr->BPB_TotSec32 / hdr->BPB_SecPerClus);
+	bytes_per_clus   = hdr->BPB_SecPerClus * hdr->BPB_BytsPerSec;
+	before_data_sec  = hdr->BPB_RsvdSecCnt + (hdr->BPB_NumFATs * hdr->BPB_FATSz32);
+	clus_begin       = (u8 *)hdr + before_data_sec * hdr->BPB_BytsPerSec;
+	clus_end         = (u8 *)hdr + hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec;
+	u32 clus_id = 2;
+	for (u8 *clus = clus_begin; clus < clus_end; clus += bytes_per_clus) {
+		clus_type[clus_id] = probe_clus_type(clus);
+		printf("%s ", idstr[clus_type[clus_id]]);
+		++clus_id;
+	}
+	// printf("%u\n", hdr->BPB_TotSec32 / hdr->BPB_SecPerClus);
 }
