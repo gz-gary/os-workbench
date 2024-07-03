307ca5b73e61b371a1753f641fbc8fdc0827386a
tracer-nju <tracer@nju.edu.cn>
2024-07-03 11:36:41 +0800

    Linux pc 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:36:41 up  2:42,  6 users,  load average: 0.25, 0.32, 0.28

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -119,6 +119,9 @@ void dump_bmp() {
 	clus_begin       = (u8 *)hdr + before_data_sec * hdr->BPB_BytsPerSec;
 	clus_end         = (u8 *)hdr + hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec;
 	printf("%u\n", before_data_sec);
+	printf("%u\n", hdr->BPB_RsvdSecCnt);
+	printf("%u\n", hdr->BPB_NumFATs);
+	printf("%u\n", hdr->BPB_FATSz32);
 	printf("%d\n", (int)((void *)clus_begin - (void *)hdr));
 	u32 clus_id = 2;
 	for (u8 *clus = clus_begin; clus < clus_end; clus += bytes_per_clus) {
