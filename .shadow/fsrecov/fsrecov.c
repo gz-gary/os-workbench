7e6aa56a0be9f43ccd845a40ec713ba39c8138de
tracer-nju <tracer@nju.edu.cn>
2024-07-04 09:25:13 +0800

    Linux pc 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:25:13 up 3 min,  4 users,  load average: 1.32, 0.90, 0.40

--- .shadow/fsrecov/fsrecov.c
+++ .shadow/fsrecov/fsrecov.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <assert.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <strings.h>
 #include <sys/mman.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 #include "fat32.h"
 
 struct fat32hdr *hdr;
@@ -18,190 +19,194 @@ u8 *clus_end;
 u32 dents_per_clus;
 u32 tot_clus;
 
-void* mmap_disk(const char *filename);
+void *mmap_disk(const char *filename);
 void dump_bmp();
 
 int main(int argc, char *argv[]) {
-	assert(argc >= 2);
-	assert(sizeof(struct fat32hdr)     == 512);
-	assert(sizeof(struct fat32dent)    == 32);
-	assert(sizeof(struct fat32ldent)   == 32);
-	assert(sizeof(struct bmp_hdr_t)    == 0x36);
-	setbuf(stdout, NULL);
+  assert(argc >= 2);
+  assert(sizeof(struct fat32hdr) == 512);
+  assert(sizeof(struct fat32dent) == 32);
+  assert(sizeof(struct fat32ldent) == 32);
+  assert(sizeof(struct bmp_hdr_t) == 0x36);
+  setbuf(stdout, NULL);
 
-	hdr = mmap_disk(argv[1]);
-	dump_bmp();
-	munmap(hdr, hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec);
-	return 0;
+  hdr = mmap_disk(argv[1]);
+  dump_bmp();
+  munmap(hdr, hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec);
+  return 0;
 }
 
 void *mmap_disk(const char *fname) {
-    int fd = open(fname, O_RDWR);
+  int fd = open(fname, O_RDWR);
 
-    if (fd < 0) {
-        goto release;
-    }
+  if (fd < 0) {
+    goto release;
+  }
 
-    off_t size = lseek(fd, 0, SEEK_END);
-    if (size < 0) {
-        goto release;
-    }
+  off_t size = lseek(fd, 0, SEEK_END);
+  if (size < 0) {
+    goto release;
+  }
 
-    struct fat32hdr *hdr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-    if (hdr == MAP_FAILED) {
-        goto release;
-    }
+  struct fat32hdr *hdr =
+      mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+  if (hdr == MAP_FAILED) {
+    goto release;
+  }
 
-    close(fd);
+  close(fd);
 
-    assert(hdr->Signature_word == 0xaa55); // this is an MBR
-    assert(hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec == size);
+  assert(hdr->Signature_word == 0xaa55);  // this is an MBR
+  assert(hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec == size);
 
-    return hdr;
+  return hdr;
 
 release:
-    perror("map disk");
-    if (fd > 0) {
-        close(fd);
-    }
-    exit(1);
+  perror("map disk");
+  if (fd > 0) {
+    close(fd);
+  }
+  exit(1);
 }
 
 clus_type_t clus_type[MAX_CLUS];
 
 clus_type_t probe_clus_type(u8 *clus) {
-	if (clus[0] == 'B' && clus[1] == 'M') return CLUS_BMPHDR;
-	int cnt_bmp = 0, cnt_zero = 0;
+  if (clus[0] == 'B' && clus[1] == 'M') return CLUS_BMPHDR;
+  int cnt_bmp = 0, cnt_zero = 0;
 
-	for (int i = 0; i < bytes_per_clus - 2; ++i) {
-		if ((clus[i] == 'B' && clus[i + 1] == 'M' && clus[i + 2] == 'P') ||
-			(clus[i] == 'b' && clus[i + 1] == 'm' && clus[i + 2] == 'p')) {
-			++cnt_bmp;
-		}
-	}
-	if (cnt_bmp >= 3) return CLUS_DENT;
+  for (int i = 0; i < bytes_per_clus - 2; ++i) {
+    if ((clus[i] == 'B' && clus[i + 1] == 'M' && clus[i + 2] == 'P') ||
+        (clus[i] == 'b' && clus[i + 1] == 'm' && clus[i + 2] == 'p')) {
+      ++cnt_bmp;
+    }
+  }
+  if (cnt_bmp >= 3) return CLUS_DENT;
 
-	for (int i = 0; i < bytes_per_clus; ++i) if (clus[i] == 0) ++cnt_zero;
-	if (cnt_zero == bytes_per_clus) return CLUS_OTHERS;
+  for (int i = 0; i < bytes_per_clus; ++i)
+    if (clus[i] == 0) ++cnt_zero;
+  if (cnt_zero == bytes_per_clus) return CLUS_OTHERS;
 
-	return CLUS_BMPDATA;
+  return CLUS_BMPDATA;
 }
 
-int ascii_printable(char ch) {
-	return ch == '\0' || (ch >= ' ' && ch <= '~');
-}
+int ascii_printable(char ch) { return ch == '\0' || (ch >= ' ' && ch <= '~'); }
 
 u8 *locate_clus(u32 clus_id) {
-	return clus_begin + (clus_id - 2) * bytes_per_clus;
+  return clus_begin + (clus_id - 2) * bytes_per_clus;
 }
 
 u32 dump_long_file_name(struct fat32ldent *ldent, char *buf) {
-	int len = 0;
-	int cnt_ldent = ldent->LDIR_Ord ^ LAST_LONG_ENTRY;
-	for (int j = cnt_ldent - 1; j >= 0; --j) {
-		if (len >= 64) break;
-		for (int k = 1; k <= 5; ++k)
-			if (ascii_printable(ldent[j].LDIR_Name1[k - 1]))
-				buf[len++] = ldent[j].LDIR_Name1[k - 1];
-		for (int k = 6; k <= 11; ++k)
-			if (ascii_printable(ldent[j].LDIR_Name2[k - 6]))
-				buf[len++] = ldent[j].LDIR_Name2[k - 6];
-		for (int k = 12; k <= 13; ++k)
-			if (ascii_printable(ldent[j].LDIR_Name3[k - 12]))
-				buf[len++] = ldent[j].LDIR_Name3[k - 12];
-	}
-	buf[len] = '\0';
+  int len = 0;
+  int cnt_ldent = ldent->LDIR_Ord ^ LAST_LONG_ENTRY;
+  for (int j = cnt_ldent - 1; j >= 0; --j) {
+    if (len >= 64) break;
+    for (int k = 1; k <= 5; ++k)
+      if (ascii_printable(ldent[j].LDIR_Name1[k - 1]))
+        buf[len++] = ldent[j].LDIR_Name1[k - 1];
+    for (int k = 6; k <= 11; ++k)
+      if (ascii_printable(ldent[j].LDIR_Name2[k - 6]))
+        buf[len++] = ldent[j].LDIR_Name2[k - 6];
+    for (int k = 12; k <= 13; ++k)
+      if (ascii_printable(ldent[j].LDIR_Name3[k - 12]))
+        buf[len++] = ldent[j].LDIR_Name3[k - 12];
+  }
+  buf[len] = '\0';
 
-	struct fat32dent *dent = (struct fat32dent *)(ldent + cnt_ldent);
-	return (dent->DIR_FstClusHI << 16) | (dent->DIR_FstClusLO);
+  struct fat32dent *dent = (struct fat32dent *)(ldent + cnt_ldent);
+  return (dent->DIR_FstClusHI << 16) | (dent->DIR_FstClusLO);
 }
 
 u32 dump_short_file_name(struct fat32dent *dent, char *buf) {
-	int len = 0;
-	for (int j = 0; j < 11; ++j) {
-		if (j == 8) buf[len++] = '.';
-		if (ascii_printable(dent->DIR_Name[j]))
-			buf[len++] = dent->DIR_Name[j];
-	}
-	buf[len] = '\0';
-	return (dent->DIR_FstClusHI << 16) | (dent->DIR_FstClusLO);
+  int len = 0;
+  for (int j = 0; j < 11; ++j) {
+    if (j == 8) buf[len++] = '.';
+    if (ascii_printable(dent->DIR_Name[j])) buf[len++] = dent->DIR_Name[j];
+  }
+  buf[len] = '\0';
+  return (dent->DIR_FstClusHI << 16) | (dent->DIR_FstClusLO);
 }
 
 void dump_bmp() {
-	bytes_per_clus   = hdr->BPB_SecPerClus * hdr->BPB_BytsPerSec;
-	before_data_sec  = hdr->BPB_RsvdSecCnt + ((hdr->BPB_NumFATs) * (hdr->BPB_FATSz32));
-	clus_begin       = (u8 *)hdr + before_data_sec * hdr->BPB_BytsPerSec;
-	clus_end         = (u8 *)hdr + hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec;
-	dents_per_clus   = bytes_per_clus / sizeof(struct fat32dent);
-	tot_clus         = (clus_end - clus_begin) / bytes_per_clus;
+  bytes_per_clus = hdr->BPB_SecPerClus * hdr->BPB_BytsPerSec;
+  before_data_sec =
+      hdr->BPB_RsvdSecCnt + ((hdr->BPB_NumFATs) * (hdr->BPB_FATSz32));
+  clus_begin = (u8 *)hdr + before_data_sec * hdr->BPB_BytsPerSec;
+  clus_end = (u8 *)hdr + hdr->BPB_TotSec32 * hdr->BPB_BytsPerSec;
+  dents_per_clus = bytes_per_clus / sizeof(struct fat32dent);
+  tot_clus = (clus_end - clus_begin) / bytes_per_clus;
 
-	u32 clus_id = 2;
-	for (u8 *clus = clus_begin; clus < clus_end; clus += bytes_per_clus) {
-		clus_type[clus_id] = probe_clus_type(clus);
-		++clus_id;
-	}
-	clus_id = 2;
-	for (u8 *clus = clus_begin; clus < clus_end; clus += bytes_per_clus) {
-		if (clus_type[clus_id] != CLUS_DENT) { ++clus_id; continue; }
-		struct fat32dent *dent = (struct fat32dent *)clus;
-		char buf[128];
-		u32 bmp_clus_id;
-		struct bmp_hdr_t *bmp_hdr;
+  u32 clus_id = 2;
+  for (u8 *clus = clus_begin; clus < clus_end; clus += bytes_per_clus) {
+    clus_type[clus_id] = probe_clus_type(clus);
+    ++clus_id;
+  }
+  clus_id = 2;
+  for (u8 *clus = clus_begin; clus < clus_end; clus += bytes_per_clus) {
+    if (clus_type[clus_id] != CLUS_DENT) {
+      ++clus_id;
+      continue;
+    }
+    struct fat32dent *dent = (struct fat32dent *)clus;
+    char buf[128];
+    u32 bmp_clus_id;
+    struct bmp_hdr_t *bmp_hdr;
 
-		for (int i = 0; i < dents_per_clus; ++i) {
-			if ((dent[i].DIR_Attr & ATTR_LONG_NAME) == ATTR_LONG_NAME) { // this entry is a 'long name directory entry' 
-				struct fat32ldent *ldent = (struct fat32ldent *)&dent[i];
-				if ((ldent->LDIR_Ord & LAST_LONG_ENTRY) == 0) continue;
-				int cnt_ldent = ldent->LDIR_Ord ^ LAST_LONG_ENTRY;
-				if (i + cnt_ldent >= dents_per_clus) continue; // cross cluster, abort
+    for (int i = 0; i < dents_per_clus; ++i) {
+      if ((dent[i].DIR_Attr & ATTR_LONG_NAME) ==
+          ATTR_LONG_NAME) {  // this entry is a 'long name directory entry'
+        struct fat32ldent *ldent = (struct fat32ldent *)&dent[i];
+        if ((ldent->LDIR_Ord & LAST_LONG_ENTRY) == 0) continue;
+        int cnt_ldent = ldent->LDIR_Ord ^ LAST_LONG_ENTRY;
+        if (i + cnt_ldent >= dents_per_clus) continue;  // cross cluster, abort
 
-				bzero(buf, 128);
-				bmp_clus_id = dump_long_file_name(ldent, buf);
-				i += cnt_ldent;
-			} else { // this entry is a 'short name directory entry'
-				if (dent[i].DIR_Name[0] == 0x00 ||
-					dent[i].DIR_Name[0] == 0xE5 ||
-					dent[i].DIR_Attr & ATTR_HIDDEN) {
-					continue;
-				}
+        bzero(buf, 128);
+        bmp_clus_id = dump_long_file_name(ldent, buf);
+        i += cnt_ldent;
+      } else {  // this entry is a 'short name directory entry'
+        if (dent[i].DIR_Name[0] == 0x00 || dent[i].DIR_Name[0] == 0xE5 ||
+            dent[i].DIR_Attr & ATTR_HIDDEN) {
+          continue;
+        }
 
-				bzero(buf, 128);
-				bmp_clus_id = dump_short_file_name(&dent[i], buf);
-			}
+        bzero(buf, 128);
+        bmp_clus_id = dump_short_file_name(&dent[i], buf);
+      }
 
-			if (!(bmp_clus_id >= 2 &&
-				  bmp_clus_id < tot_clus &&
-				  clus_type[bmp_clus_id] == CLUS_BMPHDR)) continue;
+      if (!(bmp_clus_id >= 2 && bmp_clus_id < tot_clus &&
+            clus_type[bmp_clus_id] == CLUS_BMPHDR))
+        continue;
 
-			bmp_hdr = (struct bmp_hdr_t *)locate_clus(bmp_clus_id);
-			u32 bmp_cnt_clus = bmp_hdr->BMP_FileSz / bytes_per_clus;
-			if (bmp_hdr->BMP_FileSz % bytes_per_clus > 0) ++bmp_cnt_clus;
+      bmp_hdr = (struct bmp_hdr_t *)locate_clus(bmp_clus_id);
+      u32 bmp_cnt_clus = bmp_hdr->BMP_FileSz / bytes_per_clus;
+      if (bmp_hdr->BMP_FileSz % bytes_per_clus > 0) ++bmp_cnt_clus;
 
-			// printf("%u\t%u\t%s\tW=%u\tH=%u\n", tot_clus, bmp_clus_id, buf, bmp_hdr->BMP_Width, bmp_hdr->BMP_Height);
-			char tmp_file_name[256];
-			bzero(tmp_file_name, 256);
-			sprintf(tmp_file_name, "/tmp/fsrecov/%s", buf);
-			int bmp_fd = open(tmp_file_name, O_RDWR | O_CREAT, 0666);
-			for (int j = 0; j < bmp_cnt_clus; ++j) {
-				if (bmp_clus_id + j < tot_clus) {
-					write(bmp_fd, locate_clus(bmp_clus_id + j), bytes_per_clus);
-				}
-			}
-			close(bmp_fd);
+      // printf("%u\t%u\t%s\tW=%u\tH=%u\n", tot_clus, bmp_clus_id, buf,
+      // bmp_hdr->BMP_Width, bmp_hdr->BMP_Height);
+      char tmp_file_name[256];
+      bzero(tmp_file_name, 256);
+      sprintf(tmp_file_name, "/tmp/fsrecov/%s", buf);
+      int bmp_fd = open(tmp_file_name, O_RDWR | O_CREAT, 0666);
+      for (int j = 0; j < bmp_cnt_clus; ++j) {
+        if (bmp_clus_id + j < tot_clus) {
+          write(bmp_fd, locate_clus(bmp_clus_id + j), bytes_per_clus);
+        }
+      }
+      close(bmp_fd);
 
-			char sha1sum_cmd[300];
-			char sha1sum_str[50];
-			bzero(sha1sum_cmd, 300);
-			bzero(sha1sum_str, 50);
-			sprintf(sha1sum_cmd, "/usr/bin/sha1sum %s", tmp_file_name);
-			FILE *sha1sum_fp = popen(sha1sum_cmd, "r");
-			fscanf(sha1sum_fp, "%s", sha1sum_str);
-			pclose(sha1sum_fp);
+      char sha1sum_cmd[300];
+      char sha1sum_str[50];
+      bzero(sha1sum_cmd, 300);
+      bzero(sha1sum_str, 50);
+      sprintf(sha1sum_cmd, "/usr/bin/sha1sum %s", tmp_file_name);
+      FILE *sha1sum_fp = popen(sha1sum_cmd, "r");
+      fscanf(sha1sum_fp, "%s", sha1sum_str);
+      pclose(sha1sum_fp);
 
-			// fprintf(stderr, "%s %s\n", sha1sum_str, buf);
-			printf("%s %s\n", sha1sum_str, buf);
-		}
-		// printf("%s ", idstr[clus_type[clus_id]]);
-		++clus_id;
-	}
+      // fprintf(stderr, "%s %s\n", sha1sum_str, buf);
+      printf("%s %s\n", sha1sum_str, buf);
+    }
+    // printf("%s ", idstr[clus_type[clus_id]]);
+    ++clus_id;
+  }
 }
