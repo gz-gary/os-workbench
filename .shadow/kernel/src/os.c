f7fe3fa0c5452145c74877ea81aab4c021cb2c07
tracer-nju <tracer@nju.edu.cn>
2024-07-30 14:54:18 +0800

    Linux pc 6.5.0-41-generic #41~22.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun  3 11:32:55 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:54:18 up  5:52,  3 users,  load average: 0.25, 0.20, 0.15

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -92,6 +92,7 @@ static Context* os_trap(Event ev, Context *context) {
         }
     }
     panic_on(!new_context, "No context retunred");
+    printf("%d %d\n", new_context->rax, context->rax);
     assert(!memcmp(new_context, context, sizeof(Context)));
     // assert(*new_context == *context);
     return new_context;
